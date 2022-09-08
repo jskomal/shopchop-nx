fa350069c7341db3236a66f48d49d942ec922f08
Jordan Skomal <jskomal@gmail.com>
2022-09-08 02:33:06 -0600

    Add ability to add items to the list

--- pages/create.tsx
+++ pages/create.tsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react'
 
+import Link from 'next/link'
+
 import { supabase } from '../utils'
 import { TItem } from '../types'
 import ItemToBuy from './components/ItemToBuy'
@@ -10,6 +12,15 @@ import createStyles from '../styles/create.module.css'
 function create() {
   const [items, setItems] = useState<TItem[]>([])
   const [errorText, setErrorText] = useState('\u00a0')
+  const [list, setList] = useState<TItem[]>([])
+
+  useEffect(() => {
+    fetchItems()
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('list', JSON.stringify(list))
+  }, [list])
 
   const handleErrorText = (input: string) => {
     setErrorText(input)
@@ -18,19 +29,34 @@ function create() {
     }, 3000)
   }
 
-  useEffect(() => {
-    fetchItems()
-  }, [])
-
   const fetchItems = async () => {
     let { data: Items, error } = await supabase.from('Items').select('*')
     if (error) throw Error(error.message)
     if (Items) setItems(Items)
   }
 
+  const addToList = (id: number, quantity: number) => {
+    const itemToAdd = items.find((item) => item.id === id)
+    if (itemToAdd) {
+      itemToAdd.latest_quantity_purchased = quantity
+      itemToAdd.total_quantity_purchased += quantity
+      setList((prev) => {
+        return [...prev, { ...itemToAdd }]
+      })
+      handleErrorText(`Added ${quantity} ${itemToAdd.name} to the cart!`)
+    } else {
+      handleErrorText(`Could not find item with id ${id}.`)
+    }
+  }
+
   const mappedItems = items.length ? (
     items.map((item) => (
-      <ItemToBuy handleErrorText={handleErrorText} item={item} key={item.id} />
+      <ItemToBuy
+        handleErrorText={handleErrorText}
+        item={item}
+        key={item.id}
+        addToList={addToList}
+      />
     ))
   ) : (
     <p>Loading...</p>
@@ -42,7 +68,9 @@ function create() {
       <p className={createStyles.errorText}>{errorText}</p>
       <div className={createStyles.itemContainer}>{mappedItems}</div>
       <div className={createStyles.navButtons}>
-        <button>View List</button>
+        <button>
+          <Link href='/list'>View List</Link>
+        </button>
         <button>Add new Item</button>
       </div>
     </div>
