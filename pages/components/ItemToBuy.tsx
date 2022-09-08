814454cdeadd02aa8245c6c9fb03ea8f9d0c372e
Jordan Skomal <jskomal@gmail.com>
2022-09-08 02:34:07 -0600

    Refactor Items to better use the quantity input

--- pages/components/ItemToBuy.tsx
+++ pages/components/ItemToBuy.tsx
@@ -6,16 +6,16 @@ import createStyles from '../../styles/create.module.css'
 type ItemToBuyProps = {
   item: TItem
   handleErrorText: (input: string) => void
+  addToList: (id: number, quantity: number) => void
 }
 
-function ItemToBuy({ item, handleErrorText }: ItemToBuyProps) {
-  const [quantityToBuy, setQuantityToBuy] = useState(0)
+function ItemToBuy({ item, handleErrorText, addToList }: ItemToBuyProps) {
+  const [quantityToBuy, setQuantityToBuy] = useState<number | string>('')
 
   const handleQuantity = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (parseInt(e.target.value) < 0) {
       setQuantityToBuy(0)
       handleErrorText('Quantity must be greater than 0')
-      return
     } else {
       setQuantityToBuy(parseInt(e.target.value))
     }
@@ -27,12 +27,14 @@ function ItemToBuy({ item, handleErrorText }: ItemToBuyProps) {
       <p>{item.name}</p>
       <input
         type='number'
-        name='quantityToBuy'
         value={quantityToBuy}
         onChange={handleQuantity}
         className={createStyles.quantInput}
+        placeholder='Quantity'
       />
-      <button>add to list</button>
+      <button onClick={() => addToList(item.id, parseInt(quantityToBuy.toString()))}>
+        Add To List
+      </button>
     </div>
   )
 }
