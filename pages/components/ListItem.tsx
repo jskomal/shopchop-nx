df99cff8f08e96b5d9c5f56cd8084e6faf82b454
Jordan Skomal <jskomal@gmail.com>
2022-09-09 03:03:35 -0600

    Add functionality to remove item from list

--- pages/components/ListItem.tsx
+++ pages/components/ListItem.tsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
 import { TItem } from '../../types'
 
 import listStyles from '../../styles/list.module.css'
 
 type ListItemProps = {
   item: TItem
+  removeItemFromList: (itemID: number) => void
 }
 
-function ListItem({ item }: ListItemProps) {
+function ListItem({ item, removeItemFromList }: ListItemProps) {
   return (
     <div className={listStyles.card}>
       <img src={item.img} alt={item.name} className={listStyles.img} />
@@ -15,7 +15,7 @@ function ListItem({ item }: ListItemProps) {
         <p>{item.name}</p>
         <p>Quantity: {item.latest_quantity_purchased}</p>
       </div>
-      <button>Remove from List</button>
+      <button onClick={() => removeItemFromList(item.id)}>Remove from List</button>
     </div>
   )
 }
