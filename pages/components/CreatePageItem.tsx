2ec6227197dfd18dcac18db8c7854adb22f66dd6
Jordan Skomal <jskomal@gmail.com>
2022-09-09 02:16:36 -0600

    Refactor Quantity to use listQuantity

--- pages/components/CreatePageItem.tsx
+++ pages/components/CreatePageItem.tsx
@@ -6,9 +6,15 @@ type CreatePageItemProps = {
   item: TItem
   handleErrorText: (input: string) => void
   addToList: (id: number, quantity: number) => void
+  listQuantity: number | undefined
 }
 
-function CreatePageItem({ item, handleErrorText, addToList }: CreatePageItemProps) {
+function CreatePageItem({
+  item,
+  handleErrorText,
+  addToList,
+  listQuantity,
+}: CreatePageItemProps) {
   const handleAddToList = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (parseInt(e.target.value) < 0) {
       handleErrorText('Quantity must be greater than 0')
@@ -25,11 +31,7 @@ function CreatePageItem({ item, handleErrorText, addToList }: CreatePageItemProp
         type='number'
         onChange={handleAddToList}
         className={createStyles.quantInput}
-        placeholder={
-          item.latest_quantity_purchased > 0
-            ? item.latest_quantity_purchased.toString()
-            : 'Quantity'
-        }
+        placeholder={listQuantity !== undefined ? listQuantity.toString() : 'Quantity'}
       />
     </div>
   )
