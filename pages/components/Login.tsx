d42d68103e9124cf113e3d72abb701f5f16e21ab
Jordan Skomal <jskomal@gmail.com>
2022-09-09 01:03:59 -0600

    Revert errorText Changes

--- pages/components/Login.tsx
+++ pages/components/Login.tsx
@@ -1,26 +1,45 @@
-import { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import Link from 'next/link'
 
 import loginStyles from '../../styles/Login.module.css'
-import Header from './Header'
 
 function Login() {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
   const [passwordInput, setPasswordInput] = useState('')
+  const [errorText, setErrorText] = useState('\u00a0')
+  const submitRef = useRef(null)
+
+  const handleErrorText = (input: string) => {
+    setErrorText(input)
+    setTimeout(() => {
+      setErrorText('\u00a0')
+    }, 3000)
+  }
 
   const validatePassword = (input: string) => {
     if (input === process.env.NEXT_PUBLIC_PASSWORD) {
       setIsLoggedIn(true)
+    } else {
+      handleErrorText('Incorrect Password')
     }
   }
+
+  const handleSubmit = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      validatePassword(passwordInput)
+    }
+  }
+
   return (
     <div className={loginStyles.container}>
+      <p className={loginStyles.errorText}>{errorText}</p>
       {!isLoggedIn && (
         <input
           type='password'
           placeholder='Top Secret Password'
           value={passwordInput}
           onChange={(e) => setPasswordInput(e.target.value)}
+          onKeyUp={handleSubmit}
           className={loginStyles.input}
         />
       )}
@@ -28,6 +47,7 @@ function Login() {
         <button
           className={loginStyles.button}
           onClick={() => validatePassword(passwordInput)}
+          ref={submitRef}
         >
           Submit
         </button>
