25ed662fd2fb5206e4dd5f6721d218aecbb163ac
Jordan Skomal <jskomal@gmail.com>
2022-09-09 00:52:11 -0600

    Remove Error Text from Index

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,15 +4,6 @@ import Head from 'next/head'
 import Login from './components/Login'
 
 const Home: NextPage = () => {
-  const [errorText, setErrorText] = useState('\u00a0')
-
-  const handleErrorText = (input: string) => {
-    setErrorText(input)
-    setTimeout(() => {
-      setErrorText('\u00a0')
-    }, 3000)
-  }
-
   return (
     <>
       <Head>
@@ -20,7 +11,6 @@ const Home: NextPage = () => {
         <meta name='A Shopping List App' content='Powered with Next JS and Supabase' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <p>{errorText}</p>
       <Login />
     </>
   )
