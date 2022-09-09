49d6a1c014fcd8aac6635c61364c36accd04ee5b
Jordan Skomal <jskomal@gmail.com>
2022-09-09 03:03:19 -0600

    Add function to remove item from cart

--- pages/list.tsx
+++ pages/list.tsx
@@ -1,23 +1,24 @@
 import Link from 'next/link'
 import { useState, useEffect } from 'react'
 import { TItem, TList } from '../types'
-import Header from './components/Header'
 import listStyles from '../styles/list.module.css'
 import ListItem from './components/ListItem'
 
 function list() {
   const [currentList, setCurrentList] = useState<TItem[]>([])
   const [errorText, setErrorText] = useState('\u00a0')
-  const [isCartEmpty, setisCartEmpty] = useState(true)
+  const [isCartEmpty, setIsCartEmpty] = useState(true)
   const [listName, setListName] = useState('')
   const [comment, setComment] = useState('')
 
   useEffect(() => {
+    if (currentList.length) {
+      setIsCartEmpty(false)
+    }
     const localList = sessionStorage.getItem('list')
     if (typeof localList === 'string') {
       const parse = JSON.parse(localList)
       setCurrentList(parse)
-      setisCartEmpty(false)
     } else {
       setErrorText('There are no items in the list, go add some!')
     }
@@ -30,12 +31,22 @@ function list() {
     }, 3000)
   }
 
+  const removeItemFromList = (itemID: number) => {
+    setCurrentList((prev) => {
+      const filtered = prev.filter((item) => item.id !== itemID)
+      sessionStorage.setItem('list', JSON.stringify(filtered))
+      return [...filtered]
+    })
+  }
+
   const mappedItems =
     currentList.length > 0 ? (
-      currentList.map((item) => <ListItem item={item} key={item.id} />)
+      currentList.map((item) => (
+        <ListItem item={item} key={item.id} removeItemFromList={removeItemFromList} />
+      ))
     ) : (
       <div className={listStyles.itemContainer}>
-        <p>No Items in this list!</p>
+        <p className={listStyles.noItems}>No Items in this list!</p>
         <button>
           <Link href='/create'>Go Back</Link>
         </button>
@@ -48,26 +59,32 @@ function list() {
       <div className={listStyles.itemContainer}>{mappedItems}</div>
       <div className={listStyles.endSection}>
         <div className={listStyles.endPair}>
-          <input
-            type='text'
-            value={listName}
-            onChange={(e) => setListName(e.target.value)}
-            placeholder='Name This List'
-            className={listStyles.input}
-          />
-          <textarea
-            placeholder='Comments'
-            value={comment}
-            onChange={(e) => setComment(e.target.value)}
-            className={listStyles.comment}
-          ></textarea>
+          {!isCartEmpty && (
+            <input
+              type='text'
+              value={listName}
+              onChange={(e) => setListName(e.target.value)}
+              placeholder='Name This List'
+              className={listStyles.input}
+            />
+          )}
+          {!isCartEmpty && (
+            <textarea
+              placeholder='Comments'
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+              className={listStyles.comment}
+            ></textarea>
+          )}
         </div>
-        <div className={listStyles.buttonPair}>
-          <button>
-            <Link href='/create'>Add More Items</Link>
-          </button>
-          {!isCartEmpty && <button>Save List</button>}
-        </div>
+        {!isCartEmpty && (
+          <div className={listStyles.buttonPair}>
+            <button>
+              <Link href='/create'>Add More Items</Link>
+            </button>
+            <button>Save List</button>
+          </div>
+        )}
       </div>
     </div>
   )
