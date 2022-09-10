23c179470cf1de3a3aa336400ad96b2a6bcde95c
Jordan Skomal <jskomal@gmail.com>
2022-09-10 05:03:16 -0600

    Create the list context provider and pass init value

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,27 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useState } from 'react'
+
+import { TItem } from '../types'
+import currentListContext from '../context/currentListContext'
 import Header from './components/Header'
 
+import '../styles/globals.css'
+
+type TCurrentListContext = {
+  list: TItem[]
+  setList: React.Dispatch<React.SetStateAction<TItem[]>>
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
+  const [list, setList] = useState<TItem[]>([])
+  const listBinder = { list, setList }
   return (
-    <div className='App'>
-      <Header />
-      <Component {...pageProps} />
-    </div>
+    <currentListContext.Provider value={listBinder}>
+      <div className='App'>
+        <Header />
+        <Component {...pageProps} />
+      </div>
+    </currentListContext.Provider>
   )
 }
 
